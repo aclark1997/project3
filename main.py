3db10386468447efa365be7e970b8435df9de028
alex <aclark7@github.com>
2024-04-27 15:54:24 -0600

    /film

--- main.py
+++ main.py
@@ -30,6 +30,26 @@ async def root():
     return {"message": "Hello World"}
 
 
+@app.get("/film/{id}", response_class=HTMLResponse)
+async def film(id: int):
+    with open("ui/dist/film.html") as file:
+        return file.read()
+
+@app.get("/api/v1/film/{id}")
+async def api_film(id: int):
+    Film = await auto_models.get("film")
+    result = {}
+    
+    async with AsyncSession(engine) as session:
+        film = await session.execute(select(Film).where(Film.film_id == id))
+        scalar = film.scalar()
+        result = scalar #{
+            #"title": scalar.title,
+            #"description": scalar.description,
+            #"id": scalar.film_id
+        #}
+    return result
+
 @app.get("/api/v1/films")
 async def films():
     Film = await auto_models.get("film")
--- ui/film.jsx
+++ ui/film.jsx
@@ -1,23 +1,33 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-function FilmEntry({ id, title, description }) {
-  return (
-    <p>
-      <a href={`/film/${id}`}>{title}</a>: {description}
-    </p>
-  );
-}
-
 async function main() {
   //const filmsResponse = await fetch("/api/v1/films");
   //const films = await filmsResponse.json();
 
-  //const rootElt = document.getElementById("app");
-  //const root = createRoot(rootElt);
-  root.render(
-	<p>This is the film view</p>
+  const rootElt = document.getElementById("app");
+  const root = createRoot(rootElt);
+  
+  const filmResponse = await fetch("/api/v1" +  window.location.pathname)
+  const film = await filmResponse.json();
+
+  var props = "";
+
+  for(var key in film){
+     props = props + key + ": " + film[key];
+  }
+
+
+  return  root.render(
+    Object.entries(film).map(([k,v]) => (
+      <ul>
+        <li>
+           {k}: {v}
+        </li>
+      </ul>
+    )),
   );
 }
+
 
 window.onload=main
